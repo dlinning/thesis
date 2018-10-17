6f5ebe0efd87f10eb28efdb36da61e29137f863f
dlinning <douglinning@gmail.com>
2018-10-17 09:51:17 -0500

    Split up .less file

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,96 @@
 const gulp = require("gulp"),
-    plumber = require("gulp-plumber"),
-    rename = require("gulp-rename"),
-    concat = require("gulp-concat"),
-    uglify = require("gulp-uglify-es").default,
-    babel = require("gulp-babel");
+	plumber = require("gulp-plumber"),
+	rename = require("gulp-rename"),
+	concat = require("gulp-concat"),
+	uglify = require("gulp-uglify-es").default,
+	babel = require("gulp-babel");
 
 const path = require("path");
 
 const minifycss = require("gulp-minify-css"),
-    less = require("gulp-less");
+	less = require("gulp-less");
 
 const paths = {
-    styles: {
-        in: path.join(__dirname, "./web_server/dashboard/src/styles/**/*.less"),
-        out: path.join(__dirname, "./web_server/dashboard/dist/styles/")
-    },
-    scripts: {
-        in: path.join(__dirname, "./web_server/dashboard/src/scripts/**/*.js"),
-        out: path.join(__dirname, "./web_server/dashboard/dist/scripts/")
-    },
-    react: {
-        in: path.join(__dirname, "./web_server/dashboard/src/react/**/*.jsx"),
-        out: path.join(__dirname, "./web_server/dashboard/dist/react/")
-    }
+	styles: {
+		in: path.join(__dirname, "./web_server/dashboard/src/styles/bundle.less"),
+		out: path.join(__dirname, "./web_server/dashboard/dist/styles/")
+	},
+	scripts: {
+		in: path.join(__dirname, "./web_server/dashboard/src/scripts/**/*.js"),
+		out: path.join(__dirname, "./web_server/dashboard/dist/scripts/")
+	},
+	react: {
+		in: path.join(__dirname, "./web_server/dashboard/src/react/**/*.jsx"),
+		out: path.join(__dirname, "./web_server/dashboard/dist/react/")
+	}
 };
 
 const babelPresetOptions = {
-    targets: {
-        chrome: 60,
-        android: 60,
-        safari: 9,
-        ios: 9,
-        ie: 11
-    }
+	targets: {
+		chrome: 60,
+		android: 60,
+		safari: 9,
+		ios: 9,
+		ie: 11
+	}
 };
 
 const errorHander = function(err) {
-    console.log(err.message);
+	console.log(err.message);
 };
 
 gulp.task("styles", function() {
-    gulp.src(paths.styles.in)
-        .pipe(
-            plumber({
-                errorHandler: errorHander
-            })
-        )
-        .pipe(less())
-        .pipe(gulp.dest(paths.styles.out))
-        .pipe(rename({ suffix: ".min" }))
-        .pipe(minifycss())
-        .pipe(gulp.dest(paths.styles.out));
+	gulp
+		.src(paths.styles.in)
+		.pipe(
+			plumber({
+				errorHandler: errorHander
+			})
+		)
+		.pipe(less())
+		.pipe(gulp.dest(paths.styles.out))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(minifycss())
+		.pipe(gulp.dest(paths.styles.out));
 });
 
 gulp.task("scripts", function() {
-    return gulp
-        .src(paths.scripts.in)
-        .pipe(
-            plumber({
-                errorHandler: errorHander
-            })
-        )
-        .pipe(concat("bundle.js"))
-        .pipe(babel())
-        .pipe(gulp.dest(paths.scripts.out))
-        .pipe(rename({ suffix: ".min" }))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.scripts.out));
+	return gulp
+		.src(paths.scripts.in)
+		.pipe(
+			plumber({
+				errorHandler: errorHander
+			})
+		)
+		.pipe(concat("bundle.js"))
+		.pipe(babel())
+		.pipe(gulp.dest(paths.scripts.out))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.scripts.out));
 });
 gulp.task("react", function() {
-    return gulp
-        .src(paths.react.in)
-        .pipe(
-            plumber({
-                errorHandler: errorHander
-            })
-        )
-        .pipe(concat("bundle.js"))
-        .pipe(
-            babel({
-                presets: [["@babel/preset-react", babelPresetOptions]]
-            })
-        )
-        .pipe(gulp.dest(paths.react.out))
-        .pipe(rename({ suffix: ".min" }))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.react.out));
+	return gulp
+		.src(paths.react.in)
+		.pipe(
+			plumber({
+				errorHandler: errorHander
+			})
+		)
+		.pipe(concat("bundle.js"))
+		.pipe(
+			babel({
+				presets: [["@babel/preset-react", babelPresetOptions]]
+			})
+		)
+		.pipe(gulp.dest(paths.react.out))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.react.out));
 });
 
-gulp.task("watch-all", function() {
-    gulp.watch(paths.styles.in, ["styles"]);
-    gulp.watch(paths.scripts.in, ["scripts"]);
-    gulp.watch(paths.react.in, ["react"]);
+gulp.task("default", ["styles", "scripts", "react"], function() {
+	gulp.watch(paths.styles.in, ["styles"]);
+	gulp.watch(paths.scripts.in, ["scripts"]);
+	gulp.watch(paths.react.in, ["react"]);
 });
