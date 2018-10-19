867b949b44bf6fbb4a9797cd181c83d79c366c64
dlinning <douglinning@gmail.com>
2018-10-18 20:37:32 -0500

    .

--- web_server/dashboard/src/react/Modal/Modal.jsx
+++ web_server/dashboard/src/react/Modal/Modal.jsx
@@ -1,36 +1,40 @@
 class Modal extends React.Component {
-    constructor(p) {
-        super(p);
+	constructor(p) {
+		super(p);
 
-        this.state = { open: false, content: null, title: null };
+		this.state = { open: false, content: null, title: null };
 
-        messenger.subscribe("OpenModal", payload => {
-            this.setState({ open: true, content: payload.content, title: payload.title });
-        });
+		messenger.subscribe("OpenModal", payload => {
+			this.setState({
+				open: true,
+				content: payload.content,
+				title: payload.title
+			});
+		});
 
-        messenger.subscribe("CloseModal", () => {
-            this.close();
-        });
-    }
+		messenger.subscribe("CloseModal", () => {
+			this.close();
+		});
+	}
 
-    close() {
-        this.setState({ open: false, content: null });
-    }
+	close() {
+		this.setState({ open: false, content: null, title: null });
+	}
 
-    render() {
-        let s = this.state;
-        return (
-            <div id="modal-scrim" className={s.open === true ? "open" : ""}>
-                <div id="modal">
-                    <div className="controls">
-                        <span className="title">{s.title}</span>
-                        <button title="Close Modal" onClick={() => this.close()}>
-                            <i className="fas fa-times" />
-                        </button>
-                    </div>
-                    <div id="modal-content">{s.content}</div>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		let s = this.state;
+		return (
+			<div id="modal-scrim" className={s.open === true ? "open" : ""}>
+				<div id="modal">
+					<div className="controls">
+						<span className="title">{s.title}</span>
+						<button title="Close Modal" onClick={() => this.close()}>
+							<i className="fas fa-times" />
+						</button>
+					</div>
+					<div id="modal-content">{s.content}</div>
+				</div>
+			</div>
+		);
+	}
 }
--- web_server/dashboard/src/scripts/csvBuilder.js
+++ web_server/dashboard/src/scripts/csvBuilder.js
@@ -1,38 +1,41 @@
 (() => {
-    var csvBuilder = {};
+	var csvBuilder = {};
 
-    csvBuilder.build = (el, title = "output") => {
-        if (el !== undefined) {
-            var table = el.querySelector(".table");
+	csvBuilder.build = (el, title = "output") => {
+		if (el !== undefined) {
+			var table = el.querySelector(".table");
 
-            if (table !== undefined) {
-                console.log(`CsvBuilder Running for element with id=${el.id}`);
+			if (table !== undefined) {
+				console.log(`CsvBuilder Running for element with id=${el.id}`);
 
-                var startTime = Date.now();
-                var csv = "";
+				var startTime = Date.now();
+				var csv = "";
 
-                table.childNodes.forEach(row => {
-                    var rowCsv = "";
-                    row.childNodes.forEach(cell => {
-                        let val = cell.getAttribute("alt") || cell.innerText;
-                        rowCsv += val.toString().trim() + ",";
-                    });
-                    console.log(rowCsv);
+				table.childNodes.forEach(row => {
+					var rowCsv = "";
+					row.childNodes.forEach(cell => {
+						let val = cell.getAttribute("alt") || cell.innerText;
+						rowCsv += val.toString().trim() + ",";
+					});
 
-                    csv += rowCsv.slice(0, -1) + "\n";
-                });
+					csv += rowCsv.slice(0, -1) + "\n";
+				});
 
-                var endTime = Date.now() - startTime;
+				var endTime = Date.now() - startTime;
 
-                console.log(`CSV Generated in ${endTime}ms`);
+				console.log(`CSV Generated in ${endTime}ms`);
 
-                var filename = title + ".csv";
-                fileDownloader.downloadToDevice(csv, { type: "text/plain;charset=utf-8" }, filename);
-            } else {
-                console.error("Error: No .table exists as child of DataTable");
-            }
-        }
-    };
+				var filename = title + ".csv";
+				fileDownloader.downloadToDevice(
+					csv,
+					{ type: "text/plain;charset=utf-8" },
+					filename
+				);
+			} else {
+				console.error("Error: No .table exists as child of DataTable");
+			}
+		}
+	};
 
-    window.csvBuilder = csvBuilder;
+	window.csvBuilder = csvBuilder;
 })();
