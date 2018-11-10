9297ff525c3eeffe3a34b9068a731aea25be8f1f
dlinning <douglinning@gmail.com>
2018-11-09 22:20:15 -0600

    Work on migrating from sequelize

---
+++ common/helpers/new_dbhelper.js
@@ -0,0 +1,229 @@
+// Import the config file
+const config = require("../../web_server/config.json");
+const path = require("path");
+
+// Import and Setup helper functions
+const uuidv4 = require("uuid/v4");
+const newUUID = () => uuidv4().toString();
+const dateAsUnixTimestamp = (d = new Date()) => Math.round(d.getTime() / 1000);
+
+// Setup DB requires / exit handling
+const Database = require("better-sqlite3");
+const db = new Database(path.join(__dirname, "..", config.db.filepath), { readonly: false });
+process.on("exit", () => db.close());
+
+// Will build the tables of the database
+// if they do not exist.
+var setupRun = false;
+module.exports.setup = () => {
+    if (!setupRun) {
+        const createSensors = db.prepare(
+            `CREATE TABLE IF NOT EXISTS "Sensors" ( "id" UUID, "name" TEXT NOT NULL, "dataType" TEXT NOT NULL, "createdAt" DATETIME NOT NULL, "updatedAt" DATETIME NOT NULL, PRIMARY KEY("id") )`
+        );
+        const createGroups = db.prepare(
+            `CREATE TABLE IF NOT EXISTS "Groups" ("id" UUID PRIMARY KEY, "name" VARCHAR(255) NOT NULL, "createdAt" DATETIME NOT NULL, "updatedAt" DATETIME NOT NULL)`
+        );
+        const createLogEntries = db.prepare(
+            `CREATE TABLE IF NOT EXISTS "LogEntries" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "timestamp" DATETIME NOT NULL, "value" VARCHAR(255) NOT NULL, "createdAt" DATETIME NOT NULL, "updatedAt" DATETIME NOT NULL, "SensorId" UUID REFERENCES "Sensors" ("id") ON DELETE SET NULL ON UPDATE CASCADE)`
+        );
+        const createSensorsGroups = db.prepare(
+            `CREATE TABLE IF NOT EXISTS "SensorGroups" ("createdAt" DATETIME NOT NULL, "updatedAt" DATETIME NOT NULL, "SensorId" UUID NOT NULL REFERENCES "Sensors" ("id") ON DELETE CASCADE ON UPDATE CASCADE, "GroupId" UUID NOT NULL REFERENCES "Groups" ("id") ON DELETE CASCADE ON UPDATE CASCADE, PRIMARY KEY ("SensorId", "GroupId"))`
+        );
+        const createSettings = db.prepare(`CREATE TABLE IF NOT EXISTS "Settings" ( "key" TEXT, "value" TEXT, PRIMARY KEY("key") )`);
+
+        const setupTransaction = db.transaction(() => {
+            createSensors.run();
+            createGroups.run();
+            createLogEntries.run();
+            createSensorsGroups.run();
+            createSettings.run();
+        });
+
+        try {
+            setupTransaction();
+        } catch (err) {
+            console.error(err);
+            exit(1);
+        }
+
+        setupRun = true;
+    }
+};
+
+// Checks an object (or returned DB row) to
+// see if all of its values are null.
+// Returns false if at least one property != null
+//
+function propertiesAllNull(obj) {
+    for (var key in obj) {
+        if (obj[key] !== null && obj[key] != "") return false;
+    }
+    return true;
+}
+
+// Allows data to be aggregated into one object,
+// based off of a key.
+//
+// If `alpha = { A: {C: true}, B: {C: false} }` then
+// `bravo = groupBy( [  {ID: "A", data: [1,2,3,4,5]},
+//                      { ID: "B", data: [7, 8, 9] }
+//                   ], "ID", "PAYLOAD", true, alpha )`
+// would result in `bravo = {
+//                      A: { C: true, PAYLOAD: [1, 2, 3, 4, 5] },
+//                      B: { C: false, PAYLOAD: [7, 8, 9] }
+//                  }`
+//
+function groupBy(rows, fieldName, asKey, skipNull = true, prebuilt = {}) {
+    var res = prebuilt;
+
+    for (var n = 0, l = rows.length; n < l; n++) {
+        let group = rows[n][fieldName];
+        var clone = Object.assign({}, rows[n]);
+        delete clone[fieldName];
+
+        if (res[group] === undefined) {
+            res[group] = {};
+        }
+        if (res[group][asKey] === undefined) {
+            res[group][asKey] = [];
+        }
+
+        if (skipNull && propertiesAllNull(clone)) {
+            // Skip
+        } else {
+            res[group][asKey].push(clone);
+        }
+    }
+    return res;
+}
+
+// Pull `columns` (array) from `table`, with a limit of `limit` per page,
+// starting from page `page` (0-indexed).
+//
+// TODO: Be able to "WHERE" this.
+//
+module.exports.findAndCountPaginated = (table, columns, page = 0, limit = 10) => {
+    var stmt = db.prepare(`SELECT ${columns.join(",")} FROM ${table} LIMIT @limit OFFSET @offset`);
+    var count = db.prepare(`SELECT COUNT(${columns[0]}) AS total FROM ${table}`);
+
+    var rows = stmt.all({
+        limit: limit,
+        offset: page * limit
+    });
+
+    var totalRows = count.get().total;
+    return {
+        rows: rows,
+        total: totalRows,
+        totalPages: Math.ceil(totalRows / limit) - 1,
+        page: page,
+        limit: limit
+    };
+};
+
+// Returns all logs and groups for all sensors,
+// in an object keyed by each sensor's ID.
+//
+module.exports.logsAndGroupsForAllSensors = () => {
+    var logs = groupBy(getLogsForSensors.all(), "SensorId", "logs");
+
+    var logsAndGroups = groupBy(listAllSensorsGroups.all(), "SensorId", "groups", true, logs);
+
+    return logsAndGroups;
+};
+const getLogsForSensors = db.prepare(`SELECT id,timestamp,value,createdAt,SensorId FROM LogEntries`);
+const listAllSensorsGroups = db.prepare(
+    `SELECT Sensors.id as SensorId, Groups.id as GroupId, Groups.name as GroupName
+    FROM Sensors
+    LEFT JOIN SensorGroup ON SensorGroup.SensorId = Sensors.id
+    LEFT JOIN Groups ON Groups.id = SensorGroup.GroupId`
+);
+
+// Simple wrapper around `listAllSensorsGroups` query above.
+// Split off as `getLogsForSensors` may result in a large
+// payload, when all data is not needed.
+//
+module.exports.groupsForAllSensors = () => {
+    return listAllSensorsGroups.all();
+};
+
+// Given `sensorId`, will return all logs and groups
+// for the specific sensor.
+//
+module.exports.logsAndGroupsForSensorId = sensorId => {
+    var data = groupBy(getGroupsforSensor.all(sensorId), "SensorId", "groups", true);
+    groupBy(getLogsForSensor.all(sensorId), "SensorId", "logs", true, data);
+    return data;
+};
+const getGroupsforSensor = db.prepare(
+    `SELECT Groups.id as GroupId, Groups.name as GroupName, Sensors.id as SensorId
+    FROM Sensors
+    LEFT JOIN SensorGroup ON SensorGroup.SensorId = Sensors.id
+    LEFT JOIN Groups ON Groups.id = SensorGroup.GroupId
+    WHERE Sensors.id = ?`
+);
+const getLogsForSensor = db.prepare(
+    `SELECT id,timestamp,value,createdAt,SensorId FROM LogEntries
+    WHERE SensorId = ?`
+);
+
+// Simple wrapper around `getGroupsforSensor` query above.
+// Returns groupId and groupName for all groups the specific
+// sensor with ID `sensorID` belongs to.
+//
+module.exports.getGroupsForSensor = sensorId => {
+    return getGroupsforSensor.all(sensorId);
+};
+
+// Creates a new Sensor with the given `name` (or "New Sensor")
+// and `dataType` ("float","int","string","blob","uuid","datetime").
+//
+// Will return 1 if the insert was a success.
+//
+module.exports.addSensor = (name = "New Sensor", dataType) => {
+    var d1 = dateAsUnixTimestamp();
+    const newSensor = {
+        id: newUUID(),
+        name: name,
+        dataType: dataType.toLowerCase(),
+        createdAt: d1,
+        updatedAt: d1
+    };
+    // Will return 1 if the change was a success.
+    return insertSensor.run(newSensor).changes;
+};
+const insertSensor = db.prepare(
+    `INSERT INTO Sensors(id,name,dataType,createdAt,updatedAt)
+    VALUES (@id,@name,@dataType,@createdAt,@updatedAt)`
+);
+const getSensorById = db.prepare("SELECT * FROM Sensors WHERE id = ?");
+
+// Allows updating of a sensor's `name` and `dataType` columns.
+//
+// Will return an error (as a string) if the `sensorId` does
+// not exist, or a 1 if the changes were a success.
+//
+module.exports.updateSensor = (sensorId, name, dataType) => {
+    var d1 = dateAsUnixTimestamp();
+
+    var currentSensor = getSensorById(sensorId).get();
+    if (currentSensor !== undefined) {
+        currentSensor.name = name || currentSensor.name;
+        currentSensor.dataType = dataType || currentSensor.dataType;
+
+        // Change `updatedAt` always.
+        currentSensor.updatedAt = d1;
+
+        // Will return 1 if the change was a success.
+        return updateSensor.run(currentSensor).changes;
+    } else {
+        return "ERROR::SENSORID_DOES_NOT_EXIST";
+    }
+};
+const updateSensor = db.prepare(
+    `UPDATE Sensors
+    SET dataType = @dataType,
+        name = @name,
+        updatedAt = @updatedAt
+    WHERE id = @id`
+);
